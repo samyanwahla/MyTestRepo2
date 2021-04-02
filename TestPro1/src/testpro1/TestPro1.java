d98382fc3ad22e824779724cf44e24b656902c86
Samyan <samyanwahla@gmail.com>
2021-04-02 09:36:41 +0500

    Initial Project
    Created project in netbeans

---
+++ TestPro1/src/testpro1/TestPro1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testpro1;
+
+/**
+ *
+ * @author Samyan Wahla
+ */
+public class TestPro1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
